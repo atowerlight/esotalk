a5ff2aa2cf91fac452e10ca8bec14d49706fda3e
skywalker512 <houzhenhong@hotmail.com>
2018-06-28 09:02:43 +0800

    add like translation

--- addons/languages/Chinese/definitions.Likes.php
+++ addons/languages/Chinese/definitions.Likes.php
@@ -4,3 +4,5 @@ $definitions["%s likes this."] = "%s 已喜欢";
 $definitions["%s like this."] = "%s 已喜欢";
 $definitions["Unlike"] = "取消喜欢";
 $definitions["Members Who Liked This Post"] = "成员喜欢这个帖子";
+$definitions["%s and %s"] = "%s 和 %s";
+$definitions["%s others"] = "还有 %s 喜欢";
