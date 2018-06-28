faaed28e1926788aa907eea18d451b4a99d36499
skywalker512 <houzhenhong@hotmail.com>
2018-06-28 15:02:22 +0800

    add debug translatiom

---
+++ addons/languages/Chinese/definitions.Debug.php
@@ -0,0 +1,5 @@
+<?php
+$definitions["MySQL queries"] = "MySQL 请求";
+$definitions["Page loaded in %s seconds"] = "页面花费 %s 秒加载";
+$definitions["POST + GET + FILES information"] = "POST + GET + FILES 信息";
+$definitions["SESSION + COOKIE information"] = "SESSION + COOKIE 信息";
