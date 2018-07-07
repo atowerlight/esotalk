71b151b2a7000717525a8ae5b7d72a342f94db50
skywalker512 <houzhenhong@hotmail.com>
2018-07-07 20:41:28 +0800

    fix translation for wordfilters

--- addons/languages/Chinese/definitions.WordFilter.php
+++ addons/languages/Chinese/definitions.WordFilter.php
@@ -1,3 +1,3 @@
 <?php
 $definitions["Word filters"] = "文字替换";
-$definitions["message.wordFilterInstructions"] = "每一个字符为一行，格式为 <strong>要替换的文字|替换的文字</strong> ; <br>若没有输入要替换的文字则由 ** 替代。<br>英文不区分大小写，可以使用正则";
+$definitions["message.wordFilterInstructions"] = "<strong>只支持中文</strong><br>每一个字符为一行，格式为 <strong>要替换的文字|替换的文字</strong> ; <br>若没有输入要替换的文字则由 ** 替代。<br>英文不区分大小写，可以使用正则";
