fb158ddfe24f4fcfffce7d8117763046013df03e
skywalker512 <houzhenhong@hotmail.com>
2018-07-09 15:44:52 +0800

    fix bug

--- addons/plugins/Profiles/plugin.php
+++ addons/plugins/Profiles/plugin.php
@@ -97,7 +97,7 @@ class ETPlugin_Profiles extends ETPlugin {
 				"description" => "请选择你的所在地",
 				"type"        => "select",
 				"options"     => "北 京\n天 津\n河 北\n山 西\n内蒙古\n辽 宁\n吉 林\n黑龙江\n上 海\n江 苏\n浙 江\n安 徽\n福 建\n江 西\n山 东\n河 南\n湖 北\n湖 南\n广 东\n广 西
-				海 南\n重 庆\n四 川\n贵 州\n云　南\n西 藏\n陕 西\n甘　肃\n青 海\n宁 夏\n新 疆\n香 港\n澳 门\n台 湾、",
+				海 南\n重 庆\n四 川\n贵 州\n云　南\n西 藏\n陕 西\n甘　肃\n青 海\n宁 夏\n新 疆\n香 港\n澳 门\n台 湾\n国外",
 				"showOnPosts" => true,
 				"searchable"  => true,
 				"position"    => "1"
