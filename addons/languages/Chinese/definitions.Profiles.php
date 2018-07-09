4ce231a5901b89cf4a4e7dd672a67a2e69b49d12
skywalker512 <houzhenhong@hotmail.com>
2018-07-09 15:40:40 +0800

    improve Profiles translation

--- addons/languages/Chinese/definitions.Profiles.php
+++ addons/languages/Chinese/definitions.Profiles.php
@@ -1,18 +1,19 @@
 <?php
+$definitions["Profiles"] = "用户资料";
 $definitions["About"] = "关于成员";
-$definitions["Create Field"] = "创建字段";
-$definitions["Edit Field"] = "Edit Field";
+$definitions["Create Field"] = "创建选项";
+$definitions["Edit Field"] = "编辑选项";
 $definitions["Field description"] = "编辑描述";
-$definitions["Field name"] = "字段名";
+$definitions["Field name"] = "选项名";
 $definitions["Hide field from guests"] = "对游客";
-$definitions["Input type"] = "输入类型";
+$definitions["Input type"] = "选择类型";
 $definitions["Location"] = "位置";
-$definitions["Manage Profile Fields"] = "管理字段";
+$definitions["Manage Profile Fields"] = "管理选项";
 $definitions["Options"] = "选择";
 $definitions["Privacy"] = "隐私";
-$definitions["Show field on posts"] = "在话题中显示";
+$definitions["Show field on posts"] = "在帖子的顶部显示";
 $definitions["Hide field from guests"] = "对游客隐藏";
-$definitions["Searchable in member list"] = "允许会员列表中搜索";
+$definitions["Searchable in member list"] = "添加到会员列表搜索";
 $definitions["Write something about yourself."] = "写点关于你的";
 $definitions["Text"] = "单行文本";
 $definitions["Textarea"] = "长文本框";
