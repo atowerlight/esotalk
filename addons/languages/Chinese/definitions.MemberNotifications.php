61aada6ae9d98b97aa8d8061671a6f0cb014a8c3
skywalker512 <houzhenhong@hotmail.com>
2018-07-09 14:07:45 +0800

    improve members follow translation

--- addons/languages/Chinese/definitions.MemberNotifications.php
+++ addons/languages/Chinese/definitions.MemberNotifications.php
@@ -1,4 +1,5 @@
 <?php
+$definitions["Follow-m"] = "关注该用户";
 $definitions["email.postMember.body"] = "<p>你关注的<strong>%1\$s</strong>发表了一个新话题: <strong>%2\$s</strong></p><hr>%3\$s<hr><p>了解详情请点击<br>%4\$s</p>";
 $definitions["email.postMember.subject"] = "你关注的 %1\$s 发表了一个新话题";
 $definitions["Email me when there is a new post by a member I have followed"] = "我关注的用户发帖时，邮件通知我";
